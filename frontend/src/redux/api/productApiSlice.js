13919b9b9bb9ba8ba615b8d2f9edb5aa1edd46dd
ravijat2026 <ravijat2026@gmail.com>
2024-08-25 18:06:09 +0530

    productApi  modified

--- frontend/src/redux/api/productApiSlice.js
+++ frontend/src/redux/api/productApiSlice.js
@@ -83,6 +83,14 @@ export const productApiSlice = apiSlice.injectEndpoints({
             query: () => `${PRODUCT_URL}/new`,
             keepUnusedDataFor: 5,
           }),
+
+          getFilteredProducts: builder.query({
+            query: ({checked , radio}) => ({
+              url: `${PRODUCT_URL}/filtered-products`,
+              method: "POST",
+              body: {checked , radio}
+            })
+          })
       
         
     }),
@@ -100,4 +108,5 @@ export const {
   useGetTopProductsQuery,
   useGetNewProductsQuery,
   useUploadProductImageMutation,
+  useGetFilteredProductsQuery
 } = productApiSlice
