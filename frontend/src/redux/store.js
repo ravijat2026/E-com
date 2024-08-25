ed4232f0ac6b49aa72dfcaafbf9e7697a25ed7d7
ravijat2026 <ravijat2026@gmail.com>
2024-08-25 18:06:42 +0530

    store modified

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -5,11 +5,25 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { apiSlice } from "./api/apiSlice";
 
 import authReducer from './features/auth/authSlice'
+import favoritesReducer from './features/favorites/favoriteSlice'
+import { getFavoritesFromLocalStorage } from "../Utils/localStorage";
+
+import cartSliceReducer from "./features/cart/cartSlice";
+import shopSliceReducer from "./features/shop/shopSlice";
+
+const initialFavorites = getFavoritesFromLocalStorage() || [];
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer,
+        favorites: favoritesReducer,
+        cart: cartSliceReducer,
+        shop: shopSliceReducer,
+    },
+
+    preloadedState: {
+        favorites: initialFavorites
     },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
