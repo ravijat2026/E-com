2d93fae09cd6a5922d2eb1fca5a5e8481ff0224f
ravijat2026 <ravijat2026@gmail.com>
2024-09-06 01:03:36 +0530

    adding origin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    origin:"https://ecom-backend-cr0v.onrender.com",
     proxy: {
       "/api/": "https://ecom-backend-cr0v.onrender.com",
       "/uploads/": "https://ecom-backend-cr0v.onrender.com",
