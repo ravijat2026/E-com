f43985b443883b762c77138d6975f3e6163e9e2f
ravijat2026 <ravijat2026@gmail.com>
2024-09-06 01:07:31 +0530

    updating vite congif

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    origin:"https://ecom-backend-cr0v.onrender.com",
     proxy: {
-      "/api/": "https://ecom-backend-cr0v.onrender.com",
-      "/uploads/": "https://ecom-backend-cr0v.onrender.com",
+      '/api': {
+        target: 'https://ecom-backend-cr0v.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
