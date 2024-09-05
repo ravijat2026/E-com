b3c4cd76b7453c8bfdcf0e21b2ccecb433819455
ravijat2026 <ravijat2026@gmail.com>
2024-09-05 19:16:12 +0530

    gitignore file added

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 //utiles
 
@@ -23,6 +24,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors({origin: "http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 
 app.use("/api/users",userRoutes);
